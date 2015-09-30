db20d77ca362f90cb673bb73a95e83fbab623bae
Dinh Xuan Son <dinh.xuan.son@framgia.com>
2015-09-30 13:15:59 +0700

    User model

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get 'help'    => 'static_pages#help'
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
-
+  resources :users
 
 end
