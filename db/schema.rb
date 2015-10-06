558461fefd1645e2eac42b02934296790437700c
Dinh Xuan Son <dinh.xuan.son@framgia.com>
2015-10-06 14:06:00 +0700

    word list

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151001072511) do
+ActiveRecord::Schema.define(version: 20151007063833) do
+
+  create_table "answers", force: :cascade do |t|
+    t.integer  "word_id"
+    t.text     "content"
+    t.boolean  "correct_answer"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
 
   create_table "relationships", force: :cascade do |t|
     t.integer  "follower_id"
@@ -24,6 +32,17 @@ ActiveRecord::Schema.define(version: 20151001072511) do
   add_index "relationships", ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
   add_index "relationships", ["follower_id"], name: "index_relationships_on_follower_id"
 
+  create_table "user_words", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "word_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "user_words", ["user_id", "word_id"], name: "index_user_words_on_user_id_and_word_id"
+  add_index "user_words", ["user_id"], name: "index_user_words_on_user_id"
+  add_index "user_words", ["word_id"], name: "index_user_words_on_word_id"
+
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
@@ -35,5 +54,13 @@ ActiveRecord::Schema.define(version: 20151001072511) do
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
+
+  create_table "words", force: :cascade do |t|
+    t.text     "content_jp"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "words", ["content_jp"], name: "index_words_on_content_jp", unique: true
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ User.create!(name:  "Example User",
              password_confirmation: "foobar",
              admin: true)
 
-99.times do |n|
+9.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
@@ -16,7 +16,18 @@ end
 # Following relationships
 users = User.all
 user  = users.first
-following = users[2..50]
-followers = users[3..40]
+following = users[2..10]
+followers = users[3..8]
 following.each { |followed| user.follow(followed) }
 followers.each { |follower| follower.follow(user) }
+
+100.times do |n|
+  content_jp  = Faker::Lorem.word
+  word = Word.create(content_jp: content_jp)
+  answer_1 = Faker::Lorem.word
+  Answer.create(word_id: word.id, content: answer_1, correct_answer: true)
+  3.times do |n|
+    answer_1 = Faker::Lorem.word
+    Answer.create(word_id: word.id, content: answer_1)
+  end
+end
